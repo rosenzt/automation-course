c2aad7e326fccbfb5454930b9720791b3130a1d1
rosenzt <rosenzt@gmail.com>
2019-05-30 11:01:07 +0300

    fourthLessonHomeWork

---
+++ FourthLessonHomeWork/src/Author.java
@@ -0,0 +1,26 @@
+public class Author {
+
+    private String name;
+    private String address;
+
+    public Author(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }//constructor
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
