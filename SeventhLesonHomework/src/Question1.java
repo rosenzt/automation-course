99ade94cdc86fb9b6b23cea467ada64a467d034c
rosenzt <rosenzt@gmail.com>
2019-06-24 11:41:24 +0300

    Seventh lesson question 1

---
+++ SeventhLesonHomework/src/Question1.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Question1 {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/tal/automation/drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://login.salesforce.com");
+
+        driver.findElement(By.cssSelector("[name='username']")).sendKeys("use@user.com");
+        driver.findElement(By.cssSelector("[name='pw']")).sendKeys("PW123");
+        driver.findElement(By.cssSelector(".r4.fl.mr8")).click();
+
+        if (driver.findElement(By.cssSelector(".r4.fl.mr8")).isSelected()) {
+            driver.findElement(By.cssSelector("#Login")).click();
+        }
+
+        driver.close();
+    }
+
+
+}
