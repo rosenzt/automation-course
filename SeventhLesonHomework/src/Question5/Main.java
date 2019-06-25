808df1e2a71723f99465c3826e6702f6ff1bb3d4
rosenzt <rosenzt@gmail.com>
2019-06-25 15:03:05 +0300

    Question5

--- SeventhLesonHomework/src/Question5/Main.java
+++ SeventhLesonHomework/src/Question5/Main.java
@@ -1,4 +1,11 @@
 package Question5;
 
 public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(SeleniumOrg.visitPage("https://www.seleniumhq.org/"));
+        System.out.println(SeleniumOrg.isItSeleniumSite());
+
+
+    }
 }
--- SeventhLesonHomework/src/Question5/SeleniumOrg.java
+++ SeventhLesonHomework/src/Question5/SeleniumOrg.java
@@ -1,4 +1,29 @@
 package Question5;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumOrg {
+    private static WebDriver driver;
+
+    public static boolean visitPage(String URL) {
+        System.setProperty("webdriver.chrome.driver", "/Users/tal/automation/drivers/chromedriver");
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get(URL);
+
+        if (driver.getCurrentUrl().equals(URL)) {
+            return true;
+        } else return false;
+
+    }//visitPage
+
+    public static boolean isItSeleniumSite() {
+        visitPage("http://www.seleniumhq.org");
+
+        if (driver.getTitle().contains("Selenium - Web Browser Automation") || driver.getTitle().contains("Selenium")) {
+            return true;
+        } else return false;
+    }
 }
