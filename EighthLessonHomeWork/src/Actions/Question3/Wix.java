6a9d6ac2bdcaeee305a51c2072580e2b876f96ab
tal <tal@tals-mbp.mynet>
2019-07-03 07:20:23 +0300

    Cont'

--- EighthLessonHomeWork/src/Actions/Question3/Wix.java
+++ EighthLessonHomeWork/src/Actions/Question3/Wix.java
@@ -11,32 +11,46 @@ import java.util.List;
 public class Wix {
 
     public static void main(String[] args) throws Exception {
+
+        List<String> URLs = new ArrayList<>();
+        List<WebElement> javaPositions = new ArrayList<>();
+        int index = 0;
         WebDriver driver;
         System.setProperty("webdriver.chrome.driver", "/Users/tal/automation/drivers/chromedriver");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://www.wix.com/jobs/locations/tel-aviv/positions");
 
+        driver.get("https://www.wix.com/jobs/locations/tel-aviv/positions");
         Thread.sleep(3000);
-       // List<WebElement> javaPositions = new ArrayList<WebElement>();
 
         List<WebElement> list = driver.findElements(By.cssSelector(".style-jozadwanchooseFileButton > [href]"));
 
         for (WebElement w : list) {
-            driver.navigate().to(w.getAttribute("href"));
-            Thread.sleep(3000);
-            if (driver.findElement(By.cssSelector("#comp-jl21s07ninlineContent")).getText().contains("java")) {
-                //javaPositions.add(w);
-                System.out.println(w.getText());
-            }
-
-            driver.navigate().back();
-            Thread.sleep(5000);
+            URLs.add(w.getAttribute("href"));
         }
 
-//        for (WebElement i : javaPositions) {
-//            System.out.println(i.getText());
-//        }
+        try {
+            for (String url : URLs) {
+                driver.navigate().to(url);
+                index++;
+                Thread.sleep(3000);
+                if (driver.findElement(By.cssSelector("#comp-jl21s07ninlineContent")).getText().contains("java")) {
+                    javaPositions.add(list.get(URLs.indexOf(url)));
+                }
+                driver.navigate().to("https://www.wix.com/jobs/locations/tel-aviv/positions");
+                Thread.sleep(3000);
+            }
+        } catch (Exception e) {
+            System.out.println(index);
+            for (WebElement i : javaPositions) {
+                System.out.println(i.getText());
+            }
+        }
+
+        for (WebElement i : javaPositions) {
+            System.out.println(i.getText());
+        }
+
         driver.quit();
 
     }//main
